b53f99c1ff1d7af3d5bd616f375aed76110068f9
manu800 <shailendra@editorji.com>
2026-04-22 18:10:47 +0530

    apr22aaa

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ app.get('/health', (req, res) => res.json({ status: 'ok' }));
 app.use('/items', require('./routes/items'));
 app.use('/storage', require('./routes/storage'));
 
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
